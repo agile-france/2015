56ef7e6c8d29d9edb0bd8edfa5dd8fde3f3c678e
Alvin BERTHELOT <alvin.berthelot@gmail.com>
2015-03-19 11:22:18 +0100

    Concaténation et minification des CSS dans la tache de build

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
 var del = require('del');
+var es = require('event-stream');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var minifyCSS = require('gulp-minify-css');
 
 gulp.task('clean', function (cb) {
   del([
@@ -12,15 +16,21 @@ gulp.task('clean', function (cb) {
   ], cb);
 });
 
-gulp.task('sass', function () {
-  gulp.src('app/styles/*.scss')
-    .pipe(sass())
+gulp.task('css', function () {
+  // keep stream CSS after Sass pre-processing
+  var appFile = gulp.src('./app/styles/*.scss')
+    .pipe(sass());
+  // concat and minify CSS files and stream CSS
+  return es.concat(gulp.src('./app/assets/css/*.css'), appFile)
+    .pipe(concat('app.css'))
+    .pipe(minifyCSS())
+    .pipe(rename('app.min.css'))
     .pipe(gulp.dest('public/css'));
 });
 
 gulp.task('copy', function() {
-  return gulp.src('app/assets/**')
+  return gulp.src(['app/assets/**', '!app/assets/css/**'])
     .pipe(gulp.dest('public'));
 });
 
-gulp.task('build', ['clean', 'copy', 'sass']);
+gulp.task('build', ['clean', 'copy', 'css']);
